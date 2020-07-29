d0625cfa42bdd7e4ba0cbb901a6f81d4e55e7bd0
ayushkumar25 <ayush2608@gmail.com>
2020-07-29 16:00:52 +0530

    Update chapter1.py

--- chapter1.py
+++ chapter1.py
@@ -1,6 +1,28 @@
 import cv2
 # print("Imported successfully")
 
-img = cv2.imread("resources/Ayush.jpg")
+# to import image
+'''img = cv2.imread("resources/Ayush.jpg")
 cv2.imshow("Output", img)
-cv2.waitKey(0)
+cv2.waitKey(0) '''
+
+# to import video
+'''cap = cv2.VideoCapture("resources/test_video.mp4")
+cap.set(3, 640)
+cap.set(4, 480)
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break'''
+
+# to open web cam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+cap.set(10, 100)
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
